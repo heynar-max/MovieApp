f55172087f20c0bba715a8433db2f7a19dfbac4c
heynar-max <heynar76@hotmail.com>
2025-07-08 21:18:19 -0500

    Carrusel de posters

--- src/presentation/screen/home/HomeScreen.tsx
+++ src/presentation/screen/home/HomeScreen.tsx
@@ -1,14 +1,28 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import { useMovies } from '../../hook/useMovies'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { ScrollView } from 'react-native-gesture-handler'
+import { PosterCarousel } from '../../components/movies/PosterCarousel'
 
 export const HomeScreen = () => {
 
-    const {} = useMovies();
+    // para que la pagina top quede acorde a la pantalla no tan arriba 
+    const { top } = useSafeAreaInsets();
+
+    const {isLoading, nowPlaying} = useMovies();
     
     return (
-        <View>
-            <Text>HomeScreen</Text>
-        </View>
-    )
+        <ScrollView>
+            <View style={[styles.container, { marginTop: top + 20 }]}>
+                <PosterCarousel movies={ nowPlaying }/>
+            </View>
+        </ScrollView>
+        )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        paddingBottom: 30
+    }
+});
