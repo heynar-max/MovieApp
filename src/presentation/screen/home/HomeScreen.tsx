88da826e0db317639083266bb1d6fbd1abf37c55
heynar-max <heynar76@hotmail.com>
2025-07-08 21:39:13 -0500

    Carrusel de películas con FlatList

--- src/presentation/screen/home/HomeScreen.tsx
+++ src/presentation/screen/home/HomeScreen.tsx
@@ -1,21 +1,38 @@
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import { useMovies } from '../../hook/useMovies'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { ScrollView } from 'react-native-gesture-handler'
 import { PosterCarousel } from '../../components/movies/PosterCarousel'
+import { HorizontalCarousel } from '../../components/movies/HorizontalCarousel'
 
 export const HomeScreen = () => {
 
     // para que la pagina top quede acorde a la pantalla no tan arriba 
     const { top } = useSafeAreaInsets();
 
-    const {isLoading, nowPlaying} = useMovies();
+    const {isLoading, nowPlaying, popular, topRated, upcoming} = useMovies();
+
+    if (isLoading) {
+        return <Text>Cargando...</Text>
+    }
     
     return (
         <ScrollView>
             <View style={[styles.container, { marginTop: top + 20 }]}>
+
+                {/* principal  */}
                 <PosterCarousel movies={ nowPlaying }/>
+
+                {/* populares  */}
+                <HorizontalCarousel movies={popular} title='Populares'/>
+
+                {/* top Rated  */}
+                <HorizontalCarousel movies={topRated} title='Mejor Calificadas'/>
+
+                {/* populares  */}
+                <HorizontalCarousel movies={upcoming} title='Próximanente'/>
+
             </View>
         </ScrollView>
         )
